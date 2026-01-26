806d287084a419902131d5fd1874e1930f642599
Eliahu <ilyablokh@gmail.com>
2026-01-26 22:03:01 +0200

    create player entity

--- Assets/QuantumUser/Simulation/Systems/MoveCharacterSystem.cs
+++ Assets/QuantumUser/Simulation/Systems/MoveCharacterSystem.cs
@@ -3,7 +3,7 @@ using UnityEngine.Scripting;
 namespace Quantum.QuantumUser.Simulation.Systems
 {
     [Preserve]
-    public unsafe class MoveCharacterSystem : SystemMainThreadFilter<MoveCharacterSystem.Filter>
+    public unsafe class MoveCharacterSystem : SystemMainThreadFilter<MoveCharacterSystem.Filter>, ISignalOnPlayerAdded
     {
         public override void Update(Frame frame, ref Filter filter)
         {
@@ -14,6 +14,12 @@ namespace Quantum.QuantumUser.Simulation.Systems
         {
             public EntityRef Entity;
             public CharacterController3D* CharacterController;
+        }
+
+        public void OnPlayerAdded(Frame f, PlayerRef player, bool firstTime)
+        {
+            RuntimePlayer playerData = f.GetPlayerData(player);
+            f.Create(playerData.PlayerAvatar);
         }
     }
 }
