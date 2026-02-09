4f5b8f63a56b0b512196f4ecb3fcc7727cf0f8e4
Eliahu <ilyablokh@gmail.com>
2026-02-09 23:17:29 +0200

    add bullet movement, collisions, lifetime

--- Assets/QuantumUser/Simulation/Systems/BulletSystem.cs
+++ Assets/QuantumUser/Simulation/Systems/BulletSystem.cs
@@ -1,4 +1,5 @@
 using Photon.Deterministic;
+using Quantum.Physics2D;
 using UnityEngine.Scripting;
 
 namespace Quantum.QuantumUser.Simulation.Systems
@@ -10,10 +11,53 @@ namespace Quantum.QuantumUser.Simulation.Systems
         {
             public EntityRef Entity;
             public Bullet* Bullet;
+            public Transform2D* Transform;
         }
 
         public override void Update(Frame frame, ref Filter filter)
         {
+            FPVector2 nextPosition = filter.Bullet->Direction * filter.Bullet->Speed * frame.DeltaTime;
+            if (CheckForCollisions(frame, filter, nextPosition, out EntityRef entityHit))
+            {
+                frame.Destroy(filter.Entity);
+                return;
+            }
+
+            CheckBulletTimeExpiration(frame, filter);
+            
+            filter.Transform->Position += nextPosition;
+        }
+
+        private bool CheckForCollisions(Frame frame, Filter filter, FPVector2 nextPosition, out EntityRef entityHit)
+        {
+            entityHit = EntityRef.None;
+             
+            EntityRef owner = filter.Bullet->Owner;
+            Transform2D bulletTransform = frame.Get<Transform2D>(filter.Entity);
+            HitCollection collisions = frame.Physics2D.LinecastAll(
+                bulletTransform.Position,
+                bulletTransform.Position + nextPosition,
+                int.MaxValue,
+                QueryOptions.HitAll & ~QueryOptions.HitTriggers);
+
+            for (int i = 0; i < collisions.Count; i++)
+            {
+                Hit collision = collisions[i];
+                if (collision.Entity == owner || collision.Entity == filter.Entity)
+                    continue;
+                
+                entityHit = collision.Entity;
+                return true;
+            }
+            
+            return false;
+        }
+
+        private void CheckBulletTimeExpiration(Frame frame, Filter filter)
+        {
+            filter.Bullet->Time -= frame.DeltaTime;
+            if (filter.Bullet->Time <= 0)
+                frame.Destroy(filter.Entity);
         }
 
         public void CreateBullet(Frame f, EntityRef owner, WeaponData weaponData)
