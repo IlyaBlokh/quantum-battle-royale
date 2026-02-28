53003ca5197523e46e984d8c452c81db416b3a8d
Eliahu <ilyablokh@gmail.com>
2026-02-28 12:28:38 +0200

    add base pickupitem prefab, add pickup progress UI

---
+++ Assets/QuantumUser/View/PickupItemView.cs
@@ -0,0 +1,21 @@
+﻿using Quantum;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace QuantumUser.View
+{
+  public class PickupItemView : QuantumEntityViewComponent
+  {
+    [SerializeField] private Image pickupProgress;
+
+    public override void OnUpdateView()
+    {
+      if (!PredictedFrame.Exists(EntityRef)) 
+        return;
+      
+      var pickUpItem = PredictedFrame.Get<PickUpItem>(EntityRef);
+      var progress = pickUpItem.CurrentPickupTime / pickUpItem.PickupTime;
+      pickupProgress.fillAmount = progress.AsFloat;
+    }
+  }
+}
