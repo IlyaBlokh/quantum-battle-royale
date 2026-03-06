58f649dc416b4def3de9ff01a425b3960c11dd79
Eliahu <ilyablokh@gmail.com>
2026-03-06 15:52:39 +0200

    add ammo pickup item

---
+++ Assets/QuantumUser/Simulation/AssetData/PickupItems/AmmoPickupItem.cs
@@ -0,0 +1,19 @@
+﻿namespace Quantum
+{
+  public unsafe class AmmoPickupItem : PickupItemBase
+  {
+    public override void Pickup(Frame f, EntityRef entityPickedUp, EntityRef entityPickingUp)
+    {
+      if (f.Unsafe.TryGetPointer(entityPickingUp, out Weapon* weapon))
+      {
+        WeaponBase weaponData = f.FindAsset(weapon->WeaponData);
+        if (weaponData is FiringWeapon firingWeapon)
+        {
+          weapon->Ammo = firingWeapon.MaxAmmo;
+          f.Events.OnAmmoChanged(entityPickingUp, weapon->Ammo);
+          f.Destroy(entityPickedUp);
+        }
+      }
+    }
+  }
+}
