ea140d2710882b96e0584f56b6940bec34cbf7e1
Eliahu <ilyablokh@gmail.com>
2026-03-07 14:24:56 +0200

    add selectable character view, change selected prototype

--- Assets/Photon/QuantumMenu/Runtime/CharacterSelection/QuantumMenuCharacterSelection.cs
+++ Assets/Photon/QuantumMenu/Runtime/CharacterSelection/QuantumMenuCharacterSelection.cs
@@ -1,16 +1,58 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace Quantum.Menu
 {
   public class QuantumMenuCharacterSelection : QuantumMenuUIScreen
   {
-    [SerializeField] private CharacterModel[] _characterModels;
+    [SerializeField] private CharacterModel[] characterModels;
+    [SerializeField] private SelectableCharacter selectableCharacter;
+    [SerializeField] private Transform selectionParent;
+    [SerializeField] private QuantumMenuUIController _quantumMenuUIController;
+    
+    private readonly Dictionary<CharacterModel, SelectableCharacter> _selectableCharacters = new();
+    private CharacterModel _selectedCharacterModel;
+    
     public override void Awake()
     {
+      SelectableCharacter.OnCharacterSelected += OnCharacterSelected;
+      InitializeCharacterSelection();
     }
-    
+
+    private void InitializeCharacterSelection()
+    {
+      foreach (CharacterModel model in characterModels)
+      {
+        SelectableCharacter selectable = Instantiate(selectableCharacter, selectionParent);
+        selectable.Initialize(model);
+        _selectableCharacters.Add(model, selectable);
+      }
+      
+      OnCharacterSelected(characterModels[0]);
+    }
+
+    private void OnCharacterSelected(CharacterModel characterModel)
+    {
+      if (_selectedCharacterModel == characterModel)
+        return;
+      
+      if (_selectedCharacterModel != null)
+        _selectableCharacters[_selectedCharacterModel].SetSelected(false);
+      
+      _selectedCharacterModel = characterModel;
+      _selectableCharacters[_selectedCharacterModel].SetSelected(true);
+
+      _quantumMenuUIController.ConnectArgs.RuntimePlayers[0].PlayerAvatar = _selectedCharacterModel.EntityPrototype;
+    }
+
     public virtual void OnBackButtonPressed() {
       Controller.Show<QuantumMenuUIMain>();
+    }
+
+    private void OnDestroy()
+    {
+      SelectableCharacter.OnCharacterSelected -= OnCharacterSelected;
     }
   }
 }
---
+++ Assets/Photon/QuantumMenu/Runtime/CharacterSelection/SelectableCharacter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Quantum.Menu
+{
+  public class SelectableCharacter : MonoBehaviour
+  {
+    [SerializeField] private Image characterImage;
+    [SerializeField] private TMP_Text characterName;
+    [SerializeField] private GameObject characterSelected;
+    
+    private CharacterModel _model;
+    
+    public static event Action<CharacterModel> OnCharacterSelected;
+
+    public void Initialize(CharacterModel characterModel)
+    {
+      _model = characterModel;
+      characterImage.sprite = _model.CharacterImage;
+      characterName.text = _model.CharacterName;
+    }
+
+    public void SetSelected(bool isSelected)
+    {
+      characterSelected.SetActive(isSelected);
+    }
+
+    public void CharacterSelected()
+    {
+      OnCharacterSelected?.Invoke(_model);
+    }
+  }
+}
